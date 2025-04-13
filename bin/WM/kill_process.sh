aee45c4b71c130ce51997d4d03a8744cf1edbea9
dari862 <dhari862@gmail.com>
2025-04-14 00:59:54 +0300

    Update README.md

--- bin/WM/kill_process.sh
+++ bin/WM/kill_process.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 __process_name="${1-}"
 pidof -q ${__process_name} && killall -9 ${__process_name}
+#__process_id="$(pgrep -f $__process_name)"
+#if [ -n "$__process_id" ];then
+#	kill -9 ${__process_id}
+#fi
