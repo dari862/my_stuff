754a2c8aafa83f4d3c66d77c32602dc08b33b11e
dari <dari@debian>
2025-11-07 14:10:54 -0500

    Update README.md

--- skel/.config/autostartscripts/welcome.sh
+++ skel/.config/autostartscripts/welcome.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+. "${__distro_display_manager_lib_path}"
 sleep 2
-bash (((__distro_path_root)))/bin/X11/yad/welcome
+if [ "$Display_server_are" = "X11" ];then
+	bash (((__distro_path_root)))/bin/X11/yad/welcome
+elif [ "$Display_server_are" = "wayland" ];then
+	bash (((__distro_path_root)))/bin/wayland/welcome
+fi
